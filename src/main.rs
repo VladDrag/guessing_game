6721788147a0543e3a11922fbb7b1ec3eccd592a
VladDrag <vddragomi@icloud.com>
2023-05-26 08:02:14 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,23 @@ use std::io;
 
 fn main() {
     println!("Welcome to my guessing game!");
-    println!("Please input your guess.");
-    println!("Please type a positive number between 1 and 100!");
+    println!("Please take a guess and type a positive number between 1 and 100!");
 
     let mut rng_gen = thread_rng();
     let secret = rng_gen.gen_range(1..100);
 
     for i in 0..8 {
-        let mut input = String::new();
+        let mut guess = String::new();
 
         io::stdin()
-            .read_line(&mut input)
+            .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess = input
+        let guess = guess
             .trim_end()
-            .parse::<u8>()
+            .parse::<i32>()
             .expect("Code broken at line 22 in main.rs.");
 
-        println!("You guessed: {}", guess);
         if guess == secret {
             println!("You win!");
             break;
@@ -29,18 +27,19 @@ fn main() {
             println!("Try again!");
             println!("You have {} tries left", 8 - i - 1);
             if i != 7 {
-                println!("Please input your guess.");
+                println!("Please guess your guess.");
             } else {
                 println!("You lose!");
                 break;
             }
         }
+
         if guess < secret {
             println!("Too small!");
         }
+
         if guess > secret {
             println!("Too big!");
         }
-        input.clear();
     }
 }

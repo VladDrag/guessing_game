7dc8b4fc6944fb4e3c3f2caa107655e70cb21d2c
VladDrag <vddragomi@icloud.com>
2023-05-26 08:43:13 +0200

    Update main.rs
    Optimize with cmp::Ordering and manage possible errors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::{thread_rng, Rng};
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -15,31 +16,27 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess = guess
-            .trim()
-            .parse::<i32>()
-            .expect("Code broken at line 22 in main.rs.");
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a positive number between 1 and 100!");
+                continue;
+            }
+        };
 
-        if guess == secret {
-            println!("You win!");
-            break;
-        } else {
-            println!("Try again!");
-            println!("You have {} tries left", 8 - i - 1);
-            if i != 7 {
-                println!("Please guess your guess.");
-            } else {
-                println!("You lose!");
+        match guess.cmp(&secret) {
+            Ordering::Equal => {
+                println!("You win!");
                 break;
             }
-        }
-
-        if guess < secret {
-            println!("Too small!");
-        }
-
-        if guess > secret {
-            println!("Too big!");
+            Ordering::Less => {
+                println!("Too small!");
+                println!("You have {} guesses left!", 7 - i)
+            }
+            Ordering::Greater => {
+                println!("Too big!");
+                println!("You have {} guesses left!", 7 - i)
+            }
         }
     }
 }
